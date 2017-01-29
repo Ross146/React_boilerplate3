24e6fefd01f3c006f185f6812918fcf2e3f572cf
Ross146 <padalk46@gmail.com>
2017-01-29 22:48:31 +0200

    simple boilerplate react + karma

---
+++ app/app.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+//load foundation
+$(document).foundation();
+
+//app css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+    <p>BoilerPlate 3</p>,
+    document.getElementById('app')
+);
+
